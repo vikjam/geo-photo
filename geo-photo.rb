6d5557dbf73297a4f29a7964e5ac1cfa570e73e1
Vikram Jambulapati <vikjam@gmail.com>
2014-05-26 22:52:09 -0400

    add example

--- geo-photo.rb
+++ geo-photo.rb
@@ -2,72 +2,76 @@
 require 'csv'
 require 'mini_exiftool'
 
-def decimal2degrees(coordinate, value, return_hash = false)
-    decimal = value.to_f
-    unit    = decimal.to_i.abs.to_s
-    minutes = "%02d"   % (decimal.abs * 60).to_i.modulo(60)
-    seconds = "%02.4f" % (decimal.abs * 3600).modulo(60)
+def decimal2degrees(coordinate, value)
+  decimal = value.to_f
+  unit    = decimal.to_i.abs.to_s
+  minutes = "%02d"   % (decimal.abs * 60).to_i.modulo(60)
+  seconds = "%02.4f" % (decimal.abs * 3600).modulo(60)
 
-    exif_info = Hash.new
+  if coordinate.downcase() == "latitude"
+    direction = decimal < 0 ? "S" : "N"
+  elsif coordinate.downcase() == "longitude"
+    direction = decimal < 0 ? "W" : "E"
+  else
+    raise "Hey, that's neither 'latitude' nor 'longitude'!"
+  end
 
-    if coordinate.downcase() == "lat"
-        direction = ""
-        if decimal < 0
-            direction = "S"
-        else
-            direction = "N"
-        end
-        exif_info["GPSLatitudeRef"] = direction
-        exif_info["GPSLatitude"]    = "#{unit}, #{minutes}, #{seconds}"
-    elsif coordinate.downcase() == "long"
-        if decimal > 0
-            direction = "E"
-        else
-            direction = "W"
-        end
-        exif_info["GPSLongitudeRef"] = direction
-        exif_info["GPSLatitude"] = "#{unit}, #{minutes}, #{seconds}"
-    end
-    
-    if return_hash
-        return exif_info
-    else
-        return "#{unit}°#{minutes}'#{seconds}\"#{direction}"
-    end
-
+  return direction, unit, minutes, seconds
 end
-
-def google2lightroom(theurl)
-    geo_str = theurl[/@.*z/]
-    dec_lat, dec_long = geo_str.gsub('@', '').split(',')[0..1]
-    deg_lat, deg_long = decimal2degrees("lat", dec_lat), decimal2degrees("long", dec_long)
-    return "#{deg_lat} #{deg_long}"
-end
-
-# puts google2lightroom("https://www.google.com/maps/search/199+Prospect+St,+Cambridge,+MA/@42.0369215,-71.6835014,8z")
-
-# CSV.read("/Users/vikjam/Desktop/Nikon/geo.csv", "r").each do | row |
-#     puts "#{row[0]}, #{row[1]}, #{google2lightroom(row[1])}"
-# end
 
 class GoogleMapsLink
+    attr_accessor :GPSLatitudeRef
+    attr_accessor :GPSLatitudeUnit
+    attr_accessor :GPSLatitudeMinutes
+    attr_accessor :GPSLatitudeSeconds
+    attr_accessor :GPSLongitudeRef
+    attr_accessor :GPSLongitudeUnit
+    attr_accessor :GPSLongitudeMinutes
+    attr_accessor :GPSLongitudeSeconds
 
   def initialize(google_url)
     @google_url = google_url
     @decimal_latitude, @decimal_longitude = @google_url[/@.*z/].gsub('@', '').split(',')[0..1]
+    @GPSLatitudeRef,
+      @GPSLatitudeUnit,
+      @GPSLatitudeMinutes,
+      @GPSLatitudeSeconds = decimal2degrees("latitude", @decimal_latitude)
+    @GPSLongitudeRef,
+      @GPSLongitudeUnit,
+      @GPSLongitudeMinutes,
+      @GPSLongitudeSeconds = decimal2degrees("longitude", @decimal_latitude)
   end
 
-  def puts_link()
-    puts "#{@decimal_latitude}, #{@decimal_longitude}"
+  def to_lightroom
+    l = "#{@GPSLatitudeUnit}°#{@GPSLatitudeMinutes}'#{@GPSLatitudeSeconds}\"#{@GPSLatitudeRef}, " + 
+        "#{@GPSLongitudeUnit}°#{@GPSLongitudeMinutes}'#{@GPSLongitudeSeconds}\"#{@GPSLongitudeRef}"
   end
-
 end
 
-g = GoogleMapsLink.new("https://www.google.com/maps/search/199+Prospect+St,+Cambridge,+MA/@42.0369215,-71.6835014,8z")
-puts g.puts_link()
+# def append_googlemaps(photo, link)
+#   photo.GPSLatitudeRef = link.GPSLatitudeRef
+#   photo.GPSLatitude = "#{link.GPSLatitudeUnit}, "    + 
+#                      "#{link.GPSLatitudeMinutes}, " +
+#                      "#{link.GPSLatitudeSeconds}"
+
+#   photo.GPSLongitudeRef = link.GPSGPSLongitudeRef
+#   photo.GPSLongitude = "#{link.GPSLongitudeUnit}, "    + 
+#                       "#{link.GPSLongitudeMinutes}, " +
+#                       "#{link.GPSLongitudeSeconds}"
+# end
+
 
 photo = MiniExiftool.new("/Users/vikjam/Desktop/Nikon/DSC_0085.NEF")
+link = GoogleMapsLink.new("https://www.google.com/maps/place/Worcester,+MA/@42.2754349,-71.808442,12z/")
+photo.GPSLatitudeRef = link.GPSLatitudeRef
+photo.GPSLatitude = "#{link.GPSLatitudeUnit}, "     + 
+                     "#{link.GPSLatitudeMinutes}, " +
+                     "#{link.GPSLatitudeSeconds}"
 
+photo.GPSLongitudeRef = link.GPSLongitudeRef
+photo.GPSLongitude = "#{link.GPSLongitudeUnit}, "    + 
+                     "#{link.GPSLongitudeMinutes}, " +
+                     "#{link.GPSLongitudeSeconds}"
 photo.save
 
 # End of script
