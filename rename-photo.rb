49770c0f7b71782c718f9e0d5915916e65519aba
Vikram Jambulapati <vikjam@gmail.com>
2023-10-01 20:18:23 -0700

    Add a tool for renaming files with file data from EXIF

--- rename-photo.rb
+++ rename-photo.rb
@@ -10,8 +10,10 @@ puts "What is the full file path to the folder?"
 file_path = gets
 file_path = file_path.chomp
 
-# Make a new folder to dump the RAWs
+# Make a new folder to dump the RAWs, JPGs and MOVs
 FileUtils.mkdir_p "#{file_path}/RAW"
+FileUtils.mkdir_p "#{file_path}/JPG"
+FileUtils.mkdir_p "#{file_path}/MOV"
 
 # Create an array to store metadata for a .csv
 rows = [["file_path", "file_extension"]]
@@ -30,8 +32,13 @@ Dir.glob("#{file_path}/*.{JPG,NEF,MOV}") do |pic_file|
     file_extension = File.extname(pic_file)
     new_file = "#{dt}-#{File.basename(pic_file)}"
 
-    if file_extension == ".NEF"
+    case file_extension
+    when ".NEF"
         FileUtils.move pic_file, "#{file_path}/RAW/#{new_file}"
+    when ".JPG"
+        FileUtils.move pic_file, "#{file_path}/JPG/#{new_file}"
+    when ".MOV"
+        FileUtils.move pic_file, "#{file_path}/MOV/#{new_file}"
     else
         FileUtils.move pic_file, "#{file_path}/#{new_file}"
     end
